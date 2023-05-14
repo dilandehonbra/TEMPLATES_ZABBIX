2b1987d1e26ef044e7c93cd821a13e26348f694e
dilandehonbra <85010706+dilandehonbra@users.noreply.github.com>
2023-05-13 23:51:46 -0300

    Update external_script_ssl.sh

--- external_script_ssl.sh
+++ external_script_ssl.sh
@@ -1,4 +1,13 @@
 #/usr/bin/env bash
+# Script para obter informações sobre a validade de certificados SSL/TLS de um determinado servidor web.
+# Modos de uso:
+# 1. expired_sum: calcula a soma do tempo de expiração de todos os certificados presentes na cadeia de certificação do servidor.
+#    Exemplo de uso: ./script.sh www.exemplo.com.br expired_sum
+# 2. certgen: retorna a data de início da validade do certificado SSL/TLS do servidor.
+#    Exemplo de uso: ./script.sh www.exemplo.com.br certgen
+# 3. certexp: retorna a data de fim da validade do certificado SSL/TLS do servidor.
+#    Exemplo de uso: ./script.sh www.exemplo.com.br certexp
+
 { IFS=$'\n'
 var=$({
         echo | openssl s_client -connect $1:443 2>/dev/null |\
